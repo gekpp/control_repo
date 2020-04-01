540781caff6b036bffe867706a18ffe9b25e2c8c
Gek <arkadyrom@gmail.com>
2020-04-01 14:23:16 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,21 +1,25 @@
 class minecraft {
+  $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar'
+  $install_dir = '/opt/minecraft'
   file_line { 'workaround-puppet-agent-6-14-ssl-issue':
       ensure => present,
       path   => "${facts['rubysitedir']}/../../vendor_ruby/puppet/type/file/source.rb",
       line   => '      client.get(url, include_system_store: true) do |response|',
       match  => '^      client.get\(url\) do \|response\|',
+      before => File["${install_dir}/minecraft_server.jar"],
   }
-  file { '/opt/minecraft':
+  file { "${install_dir}":
     ensure => directory,
   }
-  file { '/opt/minecraft/minecraft_server.jar':
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    source => $url,
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
@@ -26,5 +30,6 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']]
   }
 }

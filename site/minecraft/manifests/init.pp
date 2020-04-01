617b7c5e9972a162fa8e06cf39b89cb4bad8a88e
Gek <arkadyrom@gmail.com>
2020-04-01 13:41:16 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,4 +1,10 @@
 class minecraft {
+  file_line { 'workaround-puppet-agent-6-14-ssl-issue':
+      ensure => present,
+      path   => "${facts['rubysitedir']}/../../vendor_ruby/puppet/type/file/source.rb",
+      line   => '      client.get(url, include_system_store: true) do |response|',
+      match  => '^      client.get\(url\) do \|response\|',
+  }
   file { '/opt/minecraft':
     ensure => directory,
   }
